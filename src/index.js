70c64d89e4f5ae7aa652283da175670fa901b755
zena86 <76158579+zena86@users.noreply.github.com>
2021-02-11 21:01:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [];
+  let close = [];
+  for(let i=0; i<bracketsConfig.length; i++){
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+  let stack = [];   
+    for(let i=0; i<str.length; i++){
+      if(close.includes(str[i]) && stack.length>0 && close.indexOf(str[i]) === open.indexOf(stack[stack.length-1]))
+      {
+        stack.pop();
+      }else if(open.includes(str[i])){
+        stack.push(str[i]);
+      }else{
+        return false;
+      }   
+    }
+  return stack.length === 0; 
 }
